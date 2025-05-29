259a058a270f6be0656f406bdde2a23ca40ab5a7
John Martin <john.itvn@gmail.com>
2025-05-30 03:22:25 +0700

    Implementing Authentication Module

--- src/app/features/auth/components/register/register.component.ts
+++ src/app/features/auth/components/register/register.component.ts
@@ -6,7 +6,6 @@ import { AuthService } from '../../../../core/services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
   registerForm: FormGroup;
