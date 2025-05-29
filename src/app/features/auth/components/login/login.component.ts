f247a9ac97a9e1e53bcb9e95f258a82e764916e6
John Martin <john.itvn@gmail.com>
2025-05-30 03:21:14 +0700

    Updated login.component.ts

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -6,7 +6,6 @@ import { AuthService } from '../../../../core/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
